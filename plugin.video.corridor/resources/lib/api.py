5ad51668ffc11e5a27c89b62517c335a4fd7e723
mortael <mortael@gmail.com>
2023-10-02 00:29:30 +0200

    Login fix and 1.0.7 release

--- plugin.video.corridor/resources/lib/api.py
+++ plugin.video.corridor/resources/lib/api.py
@@ -1,25 +1,41 @@
-from . import constants
+from resources.lib.constants import ANCHOR_URL, LOGIN_URL, RELOAD_URL, APPCHECK_URL
+from resources.lib import kodi
+from urllib.parse import urlparse, parse_qs
 import requests
-from . import kodi
 
 
 def login():
     email = kodi.get_setting('email')
     password = kodi.get_setting('password')
+    session = requests.session()
+    ua = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.149 Safari/537.36'
 
-    params = {
-        'key': constants.GOOGLE_KEY,
-    }
+    url_variables = parse_qs(urlparse(ANCHOR_URL).query)
+    r = session.get(ANCHOR_URL, headers={
+        "User-Agent": ua})
+    token_1 = r.text.split("type=\"hidden\" id=\"recaptcha-token\" value=\"")[1].split("\"")[0]
 
-    payload = {
+    loginpayload = {
+        'clientType': 'CLIENT_TYPE_WEB',
         'email': email,
         'password': password,
         'returnSecureToken': True,
     }
 
-    session = requests.session()
+    data = f"v={url_variables['v'][0]}&reason=q&c={token_1}&k={url_variables['k'][0]}&co={url_variables['co'][0]}&hl=en&size=invisible"
+    headers = {
+        "User-Agent": ua,
+        "referer": r.url,
+        "Content-Type": "application/x-www-form-urlencoded"
+    }
+    r = session.post(RELOAD_URL, headers=headers, data=data)
+    rresp = r.text.split("[\"rresp\",\"")[1].split("\"")[0]
 
-    data = session.post(constants.LOGIN_URL, params=params, json=payload)
+    payload = {'recaptcha_enterprise_token': rresp}
+    r = session.post(APPCHECK_URL, json=payload)
+    token = r.json()['token']
+
+    data = session.post(LOGIN_URL, json=loginpayload, headers={'x-firebase-appcheck': token, 'user-agent': ua})
     if data.status_code != 200:
         kodi.set_setting('idtoken', 'null')
         return 'null'
--- plugin.video.corridor/resources/lib/constants.py
+++ plugin.video.corridor/resources/lib/constants.py
@@ -1,5 +1,8 @@
-LOGIN_URL = 'https://www.googleapis.com/identitytoolkit/v3/relyingparty/verifyPassword'
-GOOGLE_KEY = 'AIzaSyA4ybnBlx6M6kgFfjGP5vy4-6wlQQQe5z8'
+ANCHOR_URL = 'https://www.google.com/recaptcha/enterprise/anchor?ar=1&k=6LeL41IoAAAAAJZdOVxrDQLXoHffIxt0-lqGeig_&co=aHR0cHM6Ly93d3cuY29ycmlkb3JkaWdpdGFsLmNvbTo0NDM.&hl=en&v=pxZcVU8Dk73FyvFvdCgp2MSG&size=invisible&cb=k7yaql5jw3td'
+RELOAD_URL = 'https://www.google.com/recaptcha/enterprise/reload?k=6LeL41IoAAAAAJZdOVxrDQLXoHffIxt0-lqGeig_'
+APPCHECK_URL = 'https://content-firebaseappcheck.googleapis.com/v1/projects/watch-corridor/apps/1:390078941029:web:37dc352540c8a451f5c38c:exchangeRecaptchaEnterpriseToken?key=AIzaSyA3ndgjGDib-HOvDezM-cKl00NcncWtN50'
+LOGIN_URL = 'https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyA3ndgjGDib-HOvDezM-cKl00NcncWtN50'
+
 
 API = 'https://content-cache.watchcorridor.com/channels/v11'
 SHOWS = API + '/shows'
