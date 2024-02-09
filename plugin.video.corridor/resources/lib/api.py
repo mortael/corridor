12f2eff9040ee1902b48d21ad826a47db089f49a
mortael <mortael@gmail.com>
2024-02-09 20:11:34 +0100

    1.0.8 subtitle support

--- plugin.video.corridor/resources/lib/api.py
+++ plugin.video.corridor/resources/lib/api.py
@@ -8,19 +8,12 @@ def login():
     email = kodi.get_setting('email')
     password = kodi.get_setting('password')
     session = requests.session()
-    ua = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.149 Safari/537.36'
+    ua = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36'
 
     url_variables = parse_qs(urlparse(ANCHOR_URL).query)
     r = session.get(ANCHOR_URL, headers={
         "User-Agent": ua})
     token_1 = r.text.split("type=\"hidden\" id=\"recaptcha-token\" value=\"")[1].split("\"")[0]
-
-    loginpayload = {
-        'clientType': 'CLIENT_TYPE_WEB',
-        'email': email,
-        'password': password,
-        'returnSecureToken': True,
-    }
 
     data = f"v={url_variables['v'][0]}&reason=q&c={token_1}&k={url_variables['k'][0]}&co={url_variables['co'][0]}&hl=en&size=invisible"
     headers = {
@@ -31,9 +24,20 @@ def login():
     r = session.post(RELOAD_URL, headers=headers, data=data)
     rresp = r.text.split("[\"rresp\",\"")[1].split("\"")[0]
 
+    headers = {
+        "User-Agent": ua
+    }
+
     payload = {'recaptcha_enterprise_token': rresp}
-    r = session.post(APPCHECK_URL, json=payload)
+    r = session.post(APPCHECK_URL, json=payload, headers=headers)
     token = r.json()['token']
+
+    loginpayload = {
+        'clientType': 'CLIENT_TYPE_WEB',
+        'email': email,
+        'password': password,
+        'returnSecureToken': True,
+    }
 
     data = session.post(LOGIN_URL, json=loginpayload, headers={'x-firebase-appcheck': token, 'user-agent': ua})
     if data.status_code != 200:
--- plugin.video.corridor/resources/lib/kodi.py
+++ plugin.video.corridor/resources/lib/kodi.py
@@ -4,11 +4,14 @@ import six
 addon = xbmcaddon.Addon()
 pluginname = addon.getAddonInfo("name")
 
+
 def log(msg, level=xbmc.LOGINFO):
     xbmc.log("[{0}] {1}".format(pluginname, msg), level=level)
 
+
 def get_setting(setting):
     return addon.getSetting(setting)
+
 
 def set_setting(id, value):
     if not isinstance(value, six.string_types):
